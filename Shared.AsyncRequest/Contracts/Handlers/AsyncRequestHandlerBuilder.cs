70085e8320de9d80db1dd49cfbde835467f0e4a9
M.Rivandi <m.rivandi.gapfilm@gmail.com>
2026-05-24 23:00:05 +0400

    fix: resolve controller dependencies in a per-request scope

--- Shared.AsyncRequest/Contracts/Handlers/AsyncRequestHandlerBuilder.cs
+++ Shared.AsyncRequest/Contracts/Handlers/AsyncRequestHandlerBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Green.CT.Asyncify.Net.Contracts.Handlers;
 
@@ -7,6 +8,7 @@ internal class AsyncRequestHandlerBuilder
     private MethodInfo _method;
     private object _constructor;
     private object[] _arguments;
+    private IServiceScope _scope;
 
     public AsyncRequestHandlerBuilder WithMethod(MethodInfo method)
     {
@@ -26,11 +28,18 @@ internal class AsyncRequestHandlerBuilder
         return this;
     }
 
+    public AsyncRequestHandlerBuilder WithScope(IServiceScope scope)
+    {
+        _scope = scope;
+        return this;
+    }
+
     public IAsyncRequestHandler Build()
     {
         var asyncRequest = new Requests.AsyncRequest(_method,
             _constructor,
-            _arguments);
+            _arguments,
+            _scope);
 
         var handler = new AsyncRequestHandler(asyncRequest);
 
--- Shared.AsyncRequest/Contracts/Managers/IAsyncRequestManager.cs
+++ Shared.AsyncRequest/Contracts/Managers/IAsyncRequestManager.cs
@@ -1,13 +1,15 @@
 ﻿using System.Reflection;
 using Green.CT.Asyncify.Net.Contracts.Requests;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Green.CT.Asyncify.Net.Contracts.Managers;
 
 public interface IAsyncRequestManager
 {
-    Guid RegisterRequest(MethodInfo httpContext,
-        object endpointRequestDelegate,
-        object[] arguments);
+    Guid RegisterRequest(MethodInfo method,
+        object constructor,
+        object[] arguments,
+        IServiceScope scope);
     AsyncRequestDto Handle(Guid requestId,
         CancellationToken cancellationToken = default);
     AsyncRequestDto? GetResult(Guid requestId);
